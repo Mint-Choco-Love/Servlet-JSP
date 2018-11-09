932c7b423e253983f11ed9a3837addb008a22469
MintChoco <miraak@naver.com>
2018-11-09 10:38:23 +0900

    ,,.

--- src/com/edu/test/ServletContextTest1Servlet.java
+++ src/com/edu/test/ServletContextTest1Servlet.java
@@ -8,16 +8,20 @@ import javax.servlet.http.*;
 @WebServlet("/context1")
 public class ServletContextTest1Servlet extends HttpServlet {
 	
+	/*
 	ServletContext sc;
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		sc = config.getServletContext();
 	}
+	*/
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
+		ServletContext sc = this.getServletContext();
+		
 		out.print("Context : " + sc);
 		out.close();
 	}
